5b3f94cf03f0edaf3b8c61ee03d022ba7479e043
18dcjuarez <dcjuarez18@outlook.com>
2020-08-03 13:26:24 -0500

    First commit

---
+++ docs/precache-manifest.f60a7057b4ea66ea268cd9245dd83236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f68afc0a5b4ad455d1416de5de3e7c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5e4686a0d6f36db76b8",
+    "url": "/static/css/main.95f85376.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e4686a0d6f36db76b8",
+    "url": "/static/js/main.76d23a7d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
